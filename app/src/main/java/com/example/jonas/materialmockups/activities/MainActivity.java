f3f23509e9d69421f270773add9ab6496938987f
jonaskir <jonaskir@mail.uni-paderborn.de>
2016-05-01 17:28:36 +0200

    adds functionality for audio toolbar

--- app/src/main/java/com/example/jonas/materialmockups/activities/MainActivity.java
+++ app/src/main/java/com/example/jonas/materialmockups/activities/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.jonas.materialmockups.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,17 +14,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.jonas.materialmockups.MyFab;
 import com.example.jonas.materialmockups.R;
 import com.gordonwong.materialsheetfab.MaterialSheetFab;
 
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     MyFab fab;
     MaterialSheetFab materialSheetFab;
+
+    LinearLayout mRevealView;
+    boolean hidden = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +52,7 @@ public class MainActivity extends AppCompatActivity
         materialSheetFab = new MaterialSheetFab<>(fab, sheetView, overlay,
                 sheetColor, fabColor);
 
-        // standard FAB
-//        fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // end of FAB
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -64,6 +65,12 @@ public class MainActivity extends AppCompatActivity
 
         // select first item on startup
         navigationView.getMenu().getItem(0).setChecked(true);
+
+
+        // audio toolbar
+
+        mRevealView = (LinearLayout) findViewById(R.id.reveal_items);
+        mRevealView.setVisibility(View.INVISIBLE);
 
     }
 
@@ -97,14 +104,84 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_audio:
+
+                int cx = (mRevealView.getLeft() + mRevealView.getRight());
+//                int cy = (mRevealView.getTop() + mRevealView.getBottom())/2;
+                int cy = mRevealView.getTop();
+
+                int radius = Math.max(mRevealView.getWidth(), mRevealView.getHeight());
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+
+
+                    SupportAnimator animator =
+                            ViewAnimationUtils.createCircularReveal(mRevealView, cx, cy, 0, radius);
+                    animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                    animator.setDuration(800);
+
+                    SupportAnimator animator_reverse = animator.reverse();
+
+                    if (hidden) {
+                        mRevealView.setVisibility(View.VISIBLE);
+                        animator.start();
+                        hidden = false;
+                    } else {
+                        animator_reverse.addListener(new SupportAnimator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart() {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd() {
+                                mRevealView.setVisibility(View.INVISIBLE);
+                                hidden = true;
+
+                            }
+
+                            @Override
+                            public void onAnimationCancel() {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat() {
+
+                            }
+                        });
+                        animator_reverse.start();
+
+                    }
+                } else {
+                    if (hidden) {
+                        Animator anim = android.view.ViewAnimationUtils.createCircularReveal(mRevealView, cx, cy, 0, radius);
+                        mRevealView.setVisibility(View.VISIBLE);
+                        anim.start();
+                        hidden = false;
+
+                    } else {
+                        Animator anim = android.view.ViewAnimationUtils.createCircularReveal(mRevealView, cx, cy, radius, 0);
+                        anim.addListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                mRevealView.setVisibility(View.INVISIBLE);
+                                hidden = true;
+                            }
+                        });
+                        anim.start();
+
+                    }
+                }
+
+                return true;
+
+            case android.R.id.home:
+                supportFinishAfterTransition();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
